71ace03105e5b6eb0c12c863be06a12a568f35ca
LarinNikita <larin_nikita_m@mail.ru>
2023-11-23 22:26:24 +0700

    Фиксы пропса кнопки

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,6 +1,6 @@
 @props(['text', 'bgColor', 'click'])
 
-<button onclick="{{ $click }}"
+<button onclick="window.location='{{ $click }}'"
     class="rounded-2xl text-white whitespace-nowrap font-main text-2xl px-[50px] py-5 max-w-[380px] bg-{{ $bgColor }} relative overflow-hidden">
     {{ $text }}
 </button>
